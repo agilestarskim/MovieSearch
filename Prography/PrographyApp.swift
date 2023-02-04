0573050951748125dd01569a3187b855c703c1e9
agilestarskim <agilestarskim@naver.com>
2023-02-04 09:52:50 +0900

    Initial Commit

---
+++ Prography/PrographyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrographyApp.swift
+//  Prography
+//
+//  Created by 김민성 on 2023/02/04.
+//
+
+import SwiftUI
+
+@main
+struct PrographyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
